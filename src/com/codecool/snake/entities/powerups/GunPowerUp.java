32b6550ec049ea84d9ddef69f4cb9880f035491e
peterSzalma <petiszalma@gmail.com>
2018-11-21 10:37:38 +0100

    2 more powerups works

--- src/com/codecool/snake/entities/powerups/GunPowerUp.java
+++ src/com/codecool/snake/entities/powerups/GunPowerUp.java
@@ -1,19 +1,18 @@
 package com.codecool.snake.entities.powerups;
 
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
+import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.Snake;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import com.codecool.snake.resources.Resources;
 
 import java.util.Random;
 
-
-public class SimplePowerUp extends GameEntity implements Interactable {
+public class GunPowerUp extends GameEntity implements Interactable {
     private static Random rnd = new Random();
 
-    public SimplePowerUp() {
-        setImage(Globals.getInstance().getImage("PowerUpBerry"));
+    public GunPowerUp() {
+        setImage(Globals.getInstance().getImage("PowerUpGun"));
         double preX = rnd.nextDouble() * Globals.WINDOW_WIDTH;
         double preY = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
         if ((preX < Globals.WINDOW_WIDTH) && preX != 0) {
@@ -26,14 +25,16 @@ public class SimplePowerUp extends GameEntity implements Interactable {
 
     @Override
     public void apply(GameEntity entity) {
-        if(entity instanceof SnakeHead){
+        if (entity instanceof SnakeHead) {
             System.out.println(getMessage());
+            System.out.println(Snake.getAmmo());
             destroy();
         }
+
     }
 
     @Override
     public String getMessage() {
-        return "Got power-up :)";
+        return "Let's shoot something!";
     }
 }
--- src/com/codecool/snake/entities/powerups/SpeedUpPowerUp.java
+++ src/com/codecool/snake/entities/powerups/SpeedUpPowerUp.java
@@ -1,19 +1,17 @@
 package com.codecool.snake.entities.powerups;
 
-import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
+import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import com.codecool.snake.resources.Resources;
 
 import java.util.Random;
 
-
-public class SimplePowerUp extends GameEntity implements Interactable {
+public class SpeedUpPowerUp extends GameEntity implements Interactable {
     private static Random rnd = new Random();
 
-    public SimplePowerUp() {
-        setImage(Globals.getInstance().getImage("PowerUpBerry"));
+    public SpeedUpPowerUp() {
+        setImage(Globals.getInstance().getImage("PowerUpSpeed"));
         double preX = rnd.nextDouble() * Globals.WINDOW_WIDTH;
         double preY = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
         if ((preX < Globals.WINDOW_WIDTH) && preX != 0) {
@@ -22,18 +20,20 @@ public class SimplePowerUp extends GameEntity implements Interactable {
         if ((preY < Globals.WINDOW_HEIGHT) && preY != 0) {
             setY(preY);
         }
+
     }
 
     @Override
     public void apply(GameEntity entity) {
-        if(entity instanceof SnakeHead){
+        if (entity instanceof SnakeHead) {
             System.out.println(getMessage());
             destroy();
         }
+
     }
 
     @Override
     public String getMessage() {
-        return "Got power-up :)";
+        return "Speed up!";
     }
 }
